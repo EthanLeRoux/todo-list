70afa7ade407c42fdc002512d9f9aab3cf5a83e6
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-02-27 13:16:39 +0200

    Can delete from firebase

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,11 +3,14 @@ import {useEffect, useState} from "react";
 import {addTask, removeTask, updateTask} from "../redux/taskSlice.js";
 import {fetchTasks} from "../firebase/firebasetasks.js";
 import '/src/assets/Tasks.css';
+import {db} from '../firebase/firebase.js';
+import {collection, addDoc, deleteDoc, doc} from 'firebase/firestore';
 
 export default function Tasks() {
     const tasks = useSelector((state)=>state.tasker.tasks);
     const dispatch = useDispatch();
     const[taskToAdd, setTaskToAdd] = useState({});
+    const taskCollection = collection(db,"tasks");
 
     //local react
     const handleTaskChange = (event)=>{
@@ -26,23 +29,38 @@ export default function Tasks() {
 
     //Redux Actions
     const handleAddTask = ()=>{
+        let taskToAddToFirebase = {};
         if(taskToAdd.name ===""){
             alert("Task description cannot be left empty. Please enter a task.")
         }
         else{
+            taskToAddToFirebase = taskToAdd;
             dispatch(addTask(taskToAdd));
 
             setTaskToAdd({
                 name: '',
                 complete: false
             })
-        }
 
+            try{
+                const newTaskReference = addDoc(taskCollection,taskToAddToFirebase);
+                console.log("new task added to firebase" + newTaskReference.id)
+            }
+            catch(error){
+                console.error(error);
+            }
+        }
     }
 
     const handleRemoveTask = (taskIndex)=>{
+        deleteTaskFromFirebase(taskIndex);
         dispatch(removeTask(taskIndex));
     }
+
+    const deleteTaskFromFirebase = (taskID)=>{
+        const taskToBeDeleted = doc(db,"tasks", taskID);
+        console.log("Deleted " + taskToBeDeleted.id );
+    };
 
     const handleUpdateTask = (taskIndex, task)=>{
         handleTaskCompleted(taskIndex);
@@ -52,7 +70,7 @@ export default function Tasks() {
     return (
         <>
             <div className="task_adding_section">
-                <input type={"text"} onChange={handleTaskChange} value={taskToAdd.name} />
+                <input type={"text"} onChange={handleTaskChange} />
                 <button onClick={handleAddTask} className={"task_addbutton"}>Add task</button>
             </div>
 
@@ -60,10 +78,10 @@ export default function Tasks() {
             <ul className={"task_area"}>
                 {
                     tasks.map((task, index) => (
-                        <li key={index} className={"task_item"}>
+                        <li key={task.id} className={"task_item"}>
                             {task.name}
                             <input type={'checkbox'} value={task.completed} checked={task.completed} onChange={()=>{handleTaskCompleted(index, task)}}/>
-                            <button onClick={()=>{handleRemoveTask(index)}} className={'task_button'}>
+                            <button onClick={()=>{handleRemoveTask(task.id)}} className={'task_button'}>
                                 <img src={"src/assets/delete-icon.svg"} width={"25px"} height={"25px"} />
                             </button>
                         </li>
